4e30ff58179ac3a13157a8ab75aa3460ee680a5a
kevin Gasta <kevin.gastaldello21@gmail.com>
2022-12-04 23:47:25 -0300

    commit donde subo al repo la entrega de la clase 18

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, mostrar_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index), #ESTA ES LA FUNCION SALUDAR QUE CREAMOS PRIMERO
+    path('mi-familia/', mostrar_familiares), #NUEVA VISTA
 ]
